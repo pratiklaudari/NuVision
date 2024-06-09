a820802765a81e56a4dcc5281e3c89a52ad1b7be
prafulsapkota <64880155+prafulsapkota@users.noreply.github.com>
2024-06-09 15:07:43 +0545

    Add files via upload
    added the react chat app

---
+++ react-chat/src/components/list/chatList/Chatlist.jsx
@@ -0,0 +1,95 @@
+import "./chatList.css"
+const Chatlist = () => {
+    return (
+        <div className='chatList'>
+            <div className="search">
+                <div className="searchBar">
+                    <img src='./search.png' alt=''></img>
+                    <input type="text" placeholder="search" />
+                </div>
+                <img src='./plus.png' alt='' className="add"/>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+            
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span>
+                        Username1
+                    </span>
+                    <p>hello</p>
+                </div>
+            </div>
+            
+        </div>)
+}
+export default Chatlist
